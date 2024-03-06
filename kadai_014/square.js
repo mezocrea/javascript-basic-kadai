f2ef09ae012263187a5f6fead0adb58d62b8d47e
mezocrea <takahashi@mezocrea.com>
2024-03-06 18:31:48 +0900

    kadai_014提出

---
+++ kadai_014/square.js
@@ -0,0 +1,16 @@
+// 関数の定義（アロー関数）
+const square = (num) => {
+  return num * num;
+}
+
+// 戻り値の出力
+console.log(square(100));
+
+
+// 関数の定義（関数宣言）
+function funcSquare(number) {
+  return number * number;
+}
+
+// 戻り値の出力
+console.log(funcSquare(10));
